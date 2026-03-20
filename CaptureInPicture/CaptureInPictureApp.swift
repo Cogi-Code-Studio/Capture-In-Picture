6677b65012a04b7ed3e2a803e37377aae442d86d
RyuWoong <RyuWoong.dev@gmail.com>
2026-03-20 09:35:44 +0900

    Initial Commit

---
+++ CaptureInPicture/CaptureInPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureInPictureApp.swift
+//  CaptureInPicture
+//
+//  Created by RyuWoong on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct CaptureInPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
